83a654c5d664e1c38c7fd25d72b92a0f76fbff6c
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-03-25 16:14:31 -0400

    All slices of state should be defined now

--- adv-form/src/components/Form.js
+++ adv-form/src/components/Form.js
@@ -1,1 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import * as yup from "yup";
+import {
+  Container,
+  Card,
+  CardBody,
+  Col,
+  Row,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
+
+export default function MyForm() {
+  // state for whether button is disabled or not
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  // state for our form inputs
+  const [formState, setFormState] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    password: "",
+    role: ""
+  });
+
+  // state for errors
+  const [errors, setErrors] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    password: "",
+    role: ""
+  });
+
+  // state to set our post request to
+  const [post, setPost] = useState([]);
+}
