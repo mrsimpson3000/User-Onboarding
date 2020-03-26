7493131414dc057d6eea593723b3a537ffe25468
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-03-25 22:13:54 -0400

    3 of 4 stretches and MVP achieved. I may try to get the 4th stretch in the morning as well as having the users render to cards.

--- adv-form/src/components/Form.js
+++ adv-form/src/components/Form.js
@@ -28,7 +28,7 @@ const formSchema = yup.object().shape({
       "Password needs 1 uppercase, 1 lowercase letter and 1 number"
     )
     .required("Please enter a password")
-    .min(8, "Password needs to be 8 characters long"),
+    .min(8, "Password needs to be at least 8 characters long"),
   confirm: yup
     .string()
     .required("Please re-type password")
@@ -64,15 +64,19 @@ export default function MyForm() {
   });
 
   // state to set our post request to
-  const [post, setPost] = useState([]);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    formSchema.isValid(formState).then(valid => {
+      setButtonDisabled(!valid);
+    });
+  }, [formState]);
 
   const formSubmit = event => {
     event.preventDefault();
     axios
       .post("https://reqres.in/api/users", formState)
       .then(response => {
-        setPost(response.data);
-        console.log("success", post);
+        setUsers(...users, response.data);
         // reset for if success
         setFormState({
           fname: "",
@@ -98,10 +102,9 @@ export default function MyForm() {
           [event.target.name]: ""
         });
       })
-      .catch(error => ({
-        ...errors,
-        [event.target.name]: error.errors[0]
-      }));
+      .catch(error => {
+        setErrors({ ...errors, [event.target.name]: error.errors[0] });
+      });
   };
 
   const inputChange = event => {
@@ -116,32 +119,159 @@ export default function MyForm() {
     validateChange(event);
     setFormState(newFormData);
   };
-
+  console.log(users);
   return (
     <Container>
-      <Card className='mt-5 bg-success'>
+      <Card className='mt-5 ml-5 mr-5 bg-success'>
         <CardBody>
           <h3 className='text-center'>
             Fill Out The Form To Add A New User To The System
           </h3>
           <hr />
-          <Form onSubmit={formSubmit} className='pt-2'>
+          <Form onSubmit={formSubmit} className='p-2'>
             <Row form>
               <Col md={6}>
                 <FormGroup>
                   <Label htmlFor='firstName'>
                     First Name
                     <Input
+                      className='pl-4 pr-4'
                       id='firstName'
                       type='text'
                       name='fname'
                       value={formState.fname}
                       onChange={inputChange}
                     />
+                    {errors.fname.length > 0 ? (
+                      <p className='text-danger'>{errors.fname}</p>
+                    ) : (
+                      <p>&nbsp;</p>
+                    )}
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md={6}>
+                <FormGroup>
+                  <Label htmlFor='lastName'>
+                    Last Name
+                    <Input
+                      className='pl-4 pr-4'
+                      id='lastName'
+                      type='text'
+                      name='lname'
+                      value={formState.lname}
+                      onChange={inputChange}
+                    />
+                    {errors.lname.length > 0 ? (
+                      <p className='text-danger'>{errors.lname}</p>
+                    ) : (
+                      <p>&nbsp;</p>
+                    )}
                   </Label>
                 </FormGroup>
               </Col>
             </Row>
+            <Row form>
+              <Col md={6}>
+                <FormGroup>
+                  <Label htmlFor='email'>
+                    Email
+                    <Input
+                      className='pl-4 pr-4'
+                      id='email'
+                      type='email'
+                      name='email'
+                      value={formState.email}
+                      onChange={inputChange}
+                    />
+                    {errors.email.length > 0 ? (
+                      <p className='text-danger'>{errors.email}</p>
+                    ) : (
+                      <p>&nbsp;</p>
+                    )}
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md={6}>
+                <FormGroup>
+                  <Label htmlFor='Role'>
+                    Role
+                    <Input
+                      className='pl-5 pr-5'
+                      id='role'
+                      type='select'
+                      name='role'
+                      value={formState.role}
+                      onChange={inputChange}
+                    >
+                      <option value='Team Lead'>Team Lead</option>
+                      <option value='Designer'>Web Designer</option>
+                      <option value='Front End'>Front End Dev</option>
+                      <option value='Back End'>Back End Dev</option>
+                    </Input>
+                    <p>&nbsp;</p>
+                  </Label>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row form>
+              <Col md={6}>
+                <FormGroup>
+                  <Label htmlFor='password'>
+                    Password
+                    <Input
+                      className='pl-4 pr-4'
+                      id='password'
+                      type='password'
+                      name='password'
+                      value={formState.password}
+                      onChange={inputChange}
+                    />
+                  </Label>
+                  {errors.password.length > 0 ? (
+                    <p className='text-danger'>{errors.password}</p>
+                  ) : (
+                    <p>&nbsp;</p>
+                  )}
+                </FormGroup>
+              </Col>
+              <Col md={6}>
+                <FormGroup>
+                  <Label htmlFor='confirm'>
+                    Confirm Password
+                    <Input
+                      className='pl-4 pr-4'
+                      id='confirm'
+                      type='password'
+                      name='confirm'
+                      value={formState.confirm}
+                      onChange={inputChange}
+                    />
+                  </Label>
+                  <p>&nbsp;</p>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row form>
+              <Col>
+                <FormGroup>
+                  <Label htmlFor='terms' className='flexMe'>
+                    Terms & Conditions
+                    <Input
+                      id='terms'
+                      type='checkbox'
+                      name='terms'
+                      checked={formState.terms}
+                      onChange={inputChange}
+                    />
+                  </Label>
+                </FormGroup>
+              </Col>
+            </Row>
+            <pre>{JSON.stringify(users, null, 2)}</pre>
+            <Button color='primary' disabled={buttonDisabled}>
+              Submit
+            </Button>
           </Form>
         </CardBody>
       </Card>
